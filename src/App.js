9d111f02bf4bee17153711075c1d5f7a4ec048fd
Sichkar Yurii <si4kar2017@gmail.com>
2023-03-05 19:22:04 +0200

    react-website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { AppBar, Container, Toolbar, IconButton, Typography, Box, Paper, Grid } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow:1
+  },
+  menuButton: {
+    marginRight: theme.spacing(1)
+  },
+  title: {
+    flexGrow:1
+  },
+  mainFeaturesPost: {
+    position: 'relative',
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundOverlay: 'rgba(0,0,0,.3)'
+  },
+  mainFeaturesPostContent: {
+    position: 'relative',
+    padding: theme.spacing(9)
+  }
+}))
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <AppBar position='fixed'>
+        <Container fixed>
+          <Toolbar>
+            <IconButton edge='start' color='inherit' aria-label='menu' className={classes.menuButton}>
+              <MenuIcon/>
+            </IconButton>
+            <Typography variant='h6' className={classes.title}>Web Developer Blog</Typography>
+            <Box mr={3}>
+              <Button color='inherit' variant='outlined'>Log In</Button>
+            </Box>
+            <Button color='secondary' variant='contained'>Sign Up</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    
+      <main>
+        <Paper className={classes.mainFeaturesPost} style={{backgroundImage: `url(https://source.unsplash.com/random)`}}>
+          <Container fixed>
+            <div className={classes.overlay} />
+            <Grid container>
+              <Grid item md={6}>
+                <div className={classes.mainFeaturePostContent}>
+                  <Typography
+                    component='h1'
+                    variant='h3'
+                    color='inherit'
+                    gutterBottom
+                  >
+                    Web Developer Blog
+                  </Typography>
+                  <Typography
+                    variant='h5'
+                    color='inherit'
+                    paragraph
+                  >
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit.
+                    In accumsan libero lectus, in condimentum
+                    ligula imperdiet vel. Suspendisse ullamcorper,
+                    neque eu pharetra condimentum, sapien augue.
+                  </Typography>
+                  <Button variant='contained' color='secondary'>
+                    Learn more
+                  </Button>
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
+        </Paper>
+      </main>
+    </>
+  )
 }
 
-export default App;
